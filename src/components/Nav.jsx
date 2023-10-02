bb8fd386236adf929074c7249cb6ef24738e379c
shiv_bilgundi <shivbilgundiyt@gmail.com>
2023-10-02 21:50:20 +0530

    Deploy Commit

---
+++ src/components/Nav.jsx
@@ -0,0 +1,35 @@
+import { hamburger } from '../assets/icons';
+import { headerLogo } from '../assets/images';
+
+import { navLinks } from '../constants';
+
+const Nav = () => {
+  return (
+    <header className="padding-x py-8 absolute z-10 w-full">
+      <nav className="max-container flex justify-between items-center">
+        <a href="/">
+          <img src={headerLogo} alt="nikeLogo" width={130} height={29} />
+        </a>
+
+        <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
+          {navLinks.map((item) => (
+            <li key={item.label}>
+              <a
+                href={item.href}
+                className="font-montserrat leading-normal text-lg text-slate-gray"
+              >
+                {item.label}
+              </a>
+            </li>
+          ))}
+        </ul>
+
+        <div className="hidden max-lg:block">
+          <img src={hamburger} alt="Hambergurger" width={25} height={25} />
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default Nav;
---
+++ src/sections/PopularProducts.jsx
@@ -0,0 +1,28 @@
+import PopularProductCard from "../components/PopularProductCard";
+import {products} from '../constants';
+
+const PopularProducts = () => {
+  return (
+    <section className="max-container max-sm:mt-12" id="products">
+      <div className="flex flex-col justify-center items-center gap-5">
+        <h2 className="text-4xl font-palanquin font-bold">
+          Our
+          <span className="text-coral-red"> Popular </span>
+          Products
+        </h2>
+        <p className="lg:max-w-lg mt-2 text-slate-gray">
+          Experience top-noch quality and style with our sough-after selections.
+          Discover a world of comfort, design, and value
+        </p>
+      </div>
+
+      <div className="mt-16 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 sm:gap-4 gap-14">
+        {products.map((product) => (
+          <PopularProductCard key={product.name} {...product} />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default PopularProducts;
